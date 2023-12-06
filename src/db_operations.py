6c5bd5854432cb3c0cbb8ecf57a30cb10e6f750b
JoeYong01 <joeanselmyz@gmail.com>
2023-12-06 18:08:18 +0800

    added db-operations module

---
+++ src/db_operations.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+
+PG_DB_PARAMS = {
+    "host": "",
+    "user": "",
+    "password": "",
+    "database": ""
+}
+
+
+def execute_query(query: str) -> None:
+    """Takes a query as an input & executes it against a postgres database"""
+    try:
+        with psycopg2.connect(**PG_DB_PARAMS) as conn:
+            with conn.cursor() as cur:
+                cur.execute(query)
+    except psycopg2.errors as e:
+        print(f"a psycopg2 error occured: {e}")
