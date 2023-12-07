689d9548ce56b43d33cbbe84da8d4b9c3c13605c
JoeYong01 <joeanselmyz@gmail.com>
2023-12-07 16:50:26 +0800

    fixed func typo, added transcription post-processing function

--- src/llm_logic.py
+++ src/llm_logic.py
@@ -6,7 +6,7 @@ COMPLETIONS_MODEL = "gpt-3.5-turbo-1106"
 client = OpenAI()
 
 
-def trnascribe_call(audio_file) -> str:
+def transcribe_audio(audio_file) -> str:
     transcript = client.audio.transcriptions.create(
         model = TRANSCRIPTION_MODEL,
         file = audio_file
@@ -15,15 +15,22 @@ def trnascribe_call(audio_file) -> str:
     return transcript
 
 
+def post_process_transcription(input_text: str) -> str:
+    """prompt gpt to correct any possible transcription discrepencies"""
+    reponse = client.chat.completions.create(
+        model=COMPLETIONS_MODEL,
+        messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+        ]
+    )
+    return reponse['choices'][0]['message']['content']
+
+
 def call_chat_completions(input_text: str) -> str:
-    """_summary_
-
-    Args:
-        input_text (str): _description_
-
-    Returns:
-        str: _description_
-    """
+    """evaluates whether or not the customer issue has been resolved"""
     response = client.chat.completions.create(
     model=COMPLETIONS_MODEL,
     messages=[
