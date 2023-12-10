9ab02e1cd0e9961c41b9af823191e0d6e48e42a5
JoeYong01 <joeanselmyz@gmail.com>
2023-12-10 16:00:40 +0800

    updated llm_logc to accept more parameters

--- src/llm_logic.py
+++ src/llm_logic.py
@@ -1,37 +1,31 @@
-def transcribe_audio(client: str, model: str, audio_file) -> str:
+def transcribe_audio(
+    client: str,
+    model: str,
+    audio_file
+) -> str:
     """transcribes audio/speech to text using whisper"""
     with open(audio_file, "rb") as file:
         transcript = client.audio.transcriptions.create(
             model = model,
-            file = file
+            file = file,
+            response_format='text'
         )
-    
     return transcript
 
-
-def post_process_transcription(client: str, model: str, input_text: str) -> str:
-    """prompt gpt to correct any possible transcription discrepencies"""
-    response = client.chat.completions.create(
-        model=model,
-        messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-        ]
-    )
-    return response['choices'][0]['message']['content']
-
-
-def call_chat_completions(client: str, model: str, input_text: str) -> str:
+def call_completions(
+    client: str,
+    model: str,
+    prompt:str,
+    temperature: int,
+    input_text: str
+) -> bool:
     """evaluates whether or not the customer issue has been resolved"""
     response = client.chat.completions.create(
     model=model,
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-        ]
+        {"role": "system", "content": f"{prompt}"},
+        {"role": "user", "content": f"{input_text}"}
+        ],
+    temperature=temperature
     )
     return response['choices'][0]['message']['content']
