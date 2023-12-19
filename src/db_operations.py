0ac2971352ef71991bd84591816567cf8584b89f
JoeYong01 <joeanselmyz@gmail.com>
2023-12-19 18:15:20 +0800

    added logging for db_operations.py

--- src/db_operations.py
+++ src/db_operations.py
@@ -1,31 +1,59 @@
+import logging
 import sqlite3
 
 
 def execute_query(query: str) -> None:
     """executes a sqllite query"""
-    con = sqlite3.connect("db/gpt-powered-pipeline.db")
-    cur = con.cursor()
-    cur.execute(query)
-    if query.strip().upper().startswith("SELECT"):
-        result = cur.fetchall()
-        return result
-    else:
-        con.commit()
-    cur.close()
-    con.close()
+    try:
+        logging.info("running execute_query.")
+        con = sqlite3.connect("db/gpt-powered-pipeline.db")
+        cur = con.cursor()
+        cur.execute(query)
+        if query.strip().upper().startswith("SELECT"):
+            logging.debug("detected a SELECT statment.")
+            result = cur.fetchall()
+            return result
+        else:
+            logging.debug("detected a DML/DDL statement.")
+            con.commit()
+    except sqlite3.Error as e:
+        logging.exception("sqlite3 Exception in execute_query: %s", e)
+    except Exception as e:
+        logging.exception("Exception in execute_query: %s", e)
+    finally:
+        cur.close()
+        con.close()
+        logging.debug("connection is closed.")
 
-def insert_into_database(respondant_id, agent_id, is_resolved, feature_columns, call_logs, timestamp) -> None:
+
+def insert_into_database(
+    respondant_id,
+    agent_id,
+    is_resolved,
+    feature_columns,
+    call_logs,
+    timestamp
+) -> None:
     """executes a sqllite query"""
-    con = sqlite3.connect("db/gpt-powered-pipeline.db")
-    cur = con.cursor()
-    cur.execute(
-        """
-        INSERT INTO call_logs(respondant_id, agent_id, is_resolved, feature_columns, call_logs, timestamp)
-        VALUES(?, ?, ?, ?, ?, ?)
-        """,
-        (respondant_id, agent_id, is_resolved, feature_columns, call_logs, timestamp)
-    )
-    con.commit()
-    cur.close()
-    con.close()
-
+    query = """
+    INSERT INTO call_logs(respondant_id, agent_id, is_resolved, feature_columns, call_logs, timestamp)
+    VALUES(?, ?, ?, ?, ?, ?)
+    """
+    try:
+        logging.info("running function: insert_into_database.")
+        con = sqlite3.connect("db/gpt-powered-pipeline.db")
+        cur = con.cursor()
+        cur.execute(
+            query,
+            (respondant_id, agent_id, is_resolved, feature_columns, call_logs, timestamp)
+        )
+        con.commit()
+        logging.debug("Executed query: %s", query)
+    except sqlite3.Error as e:
+        logging.exception("sqlite3 Exception in insert_into_database : %s", e)
+    except Exception as e:
+        logging.exception("Exception in insert_into_database: %s", e)
+    finally:
+        cur.close()
+        con.close()
+        logging.debug("database connection is closed.")
