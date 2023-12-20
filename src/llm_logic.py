5ed0d142db44b76ad7486ed8bd0da9dc054ce154
JoeYong01 <joeanselmyz@gmail.com>
2023-12-20 18:34:26 +0800

    added logging to llm logic.py

--- src/llm_logic.py
+++ src/llm_logic.py
@@ -1,16 +1,38 @@
+"""contains functions to make calls to openai"""
+import logging
+import openai
+
 def transcribe_audio(
     client: str,
     model: str,
     audio_file
 ) -> str:
-    """transcribes audio/speech to text using whisper"""
-    with open(audio_file, "rb") as file:
-        transcript = client.audio.transcriptions.create(
-            model = model,
-            file = file,
-            response_format='text'
-        )
-    return transcript
+    """
+    Transcribes audio to text using OpenAI.
+
+    Args:
+        client (str): OpenAI client object to pass
+        model (str): model for transcription
+        audio_file (_type_): audio file to transcribe
+
+    Returns:
+        str: returns a transcription of an audio file
+    """
+    logging.info("running function: transcribe_audio.")
+    try:
+        with open(audio_file, "rb") as file:
+            logging.debug("transcribing audio file.")
+            transcript = client.audio.transcriptions.create(
+                model = model,
+                file = file,
+                response_format='text'
+            )
+            logging.debug("returning transcript.")
+            return transcript
+    except openai.OpenAIError as e:
+        logging.exception("OpenAI Exception in transcribe_audio: %e", e)
+    except Exception as e:
+        logging.exception("Exception in transcribe_audio: %e", e)
 
 def call_completions(
     client,
@@ -19,13 +41,33 @@ def call_completions(
     temperature: int,
     input_text: str
 ) -> bool:
-    """Evaluates whether the prompt against the input text & returns a boolean value"""
-    response = client.chat.completions.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": prompt},
-            {"role": "user", "content": input_text}
-        ],
-        temperature=temperature
-    )
-    return response.choices[0].message.content
+    """
+    Evaluates whether the prompt against the input text & returns a boolean value
+
+    Args:
+        client (_type_): OpenAI client object to be passed
+        model (str): model for completions
+        prompt (str): prompt for desired return value
+        temperature (int): lower values (0-0.2) are preferred to fulfil prompt conditions
+        input_text (str): transcription to be evaluated
+
+    Returns:
+        bool: 1 / 0
+    """
+    logging.info("running function: call_completions.")
+    try:
+        logging.debug("evaluating transcription.")
+        response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": input_text}
+            ],
+            temperature=temperature
+        )
+        logging.debug("returning boolean response.")
+        return response.choices[0].message.content
+    except openai.OpenAIError as e:
+        logging.exception("OpenAI exception in call_completions: %s", e)
+    except Exception as e:
+        logging.exception("Exception in call_completions: %s", e)
